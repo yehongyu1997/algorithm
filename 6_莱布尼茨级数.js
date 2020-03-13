5c0554f9094ea44d1c8064984c63b0128102a3b0
叶宏宇 <1119819596@qq.com>
2020-03-13 22:43:54 +0800

    作业6

--- 6_莱布尼茨级数.js
+++ 6_莱布尼茨级数.js
@@ -1,1 +1,22 @@
-// 由用户输入参数n，计算圆周率π
+var readline = require('readline');
+	
+//创建readline接口实例
+var  rl = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+});
+
+// question方法
+rl.question("请输入参数n:",function(n){
+    var sum = 1;
+
+for (var i = 1; i <= n; i++) {
+    var k=1;
+  for (var j = 1; j <= i; j++) {
+    k *= j/ (2 * j + 1);
+  }
+    sum += k;
+}
+console.log(2*sum);
+}
+)

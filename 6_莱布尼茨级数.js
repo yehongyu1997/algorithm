125830bf16bc8fc402c7628056c773f2d3a6e97a
叶宏宇 <1119819596@qq.com>
2020-03-13 22:50:00 +0800

    添加注释

--- 6_莱布尼茨级数.js
+++ 6_莱布尼茨级数.js
@@ -11,12 +11,16 @@ rl.question("请输入参数n:",function(n){
     var sum = 1;
 
 for (var i = 1; i <= n; i++) {
+    // 设k为1来重置
     var k=1;
+    // 通过for循环算出每个式子值
   for (var j = 1; j <= i; j++) {
     k *= j/ (2 * j + 1);
   }
+//   sum求和，我的sum是指作业等式右面的部分
     sum += k;
 }
-console.log(2*sum);
+// 顾再此输出π值时需×2
+console.log('π值为：'+2*sum);
 }
 )

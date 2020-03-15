f138039fdd9081ef15f94b0819b3e3b8b9db21a5
叶宏宇 <1119819596@qq.com>
2020-03-16 00:21:45 +0800

    添加注释

--- 16_模拟抛硬币.js
+++ 16_模拟抛硬币.js
@@ -1,9 +1,14 @@
+// 定义两个累加器，分别记录正反面的次数
 var count1=0;
 var count2=0;
+// 两个占比
 var ave1,ave2;
 for(var i =1 ;i<=1000000;i++){
+    // 通过random方法随机一个（0，1）区间的一个数，再通过round方法进行四舍五入
+    // 等于1为正面，等于0为反面
     var n =Math.round(Math.random());
     if (n==1){
+        // 正面累加器自加
          count1++;
     }else{
         count2++;

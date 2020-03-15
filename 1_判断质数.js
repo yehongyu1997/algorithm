4f1eed36166213c563121d33b5e997953c9ad3bc
叶宏宇 <1119819596@qq.com>
2020-03-15 10:10:36 +0800

    修改

--- 1_判断质数.js
+++ 1_判断质数.js
@@ -1,13 +1,11 @@
-var n=2
-for(var i=2;i<45887;i++){
-    if (45887%i==0){
-        n+=1
-    }
+var n = 2;
+for (var i = 2; i < 45887; i++) {
+  if (45887 % i == 0) {
+    n += 1;
+  }
 }
-if(n==2){
-    console.log('45887是质数');
-    
-}else{
-console.log('45887不是质数');
-
+if (n == 2) {
+  console.log("45887是质数");
+} else {
+  console.log("45887不是质数");
 }

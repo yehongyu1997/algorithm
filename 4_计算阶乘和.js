553cf599c1dc5b02a0ce1cda2548abc7b766cd82
叶宏宇 <1119819596@qq.com>
2020-03-15 15:48:48 +0800

    猜数游戏

--- 4_计算阶乘和.js
+++ 4_计算阶乘和.js
@@ -1,11 +1,11 @@
 // 题目： 计算1到9的阶乘之和
-var sum = 0;
-for (var i = 1; i <= 9; i++) {
-  var k = 1;
-//   计算每个数的阶乘
-  for (var j = 1; j <= i; j++) {
-    k = k * j;
-  } 
-  sum += k;
-}
-console.log(sum);
+// var sum = 0;
+// for (var i = 1; i <= 9; i++) {
+//   var k = 1;
+// //   计算每个数的阶乘
+//   for (var j = 1; j <= i; j++) {
+//     k = k * j;
+//   } 
+//   sum += k;
+// }
+// console.log(sum);

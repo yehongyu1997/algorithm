4c639d277a3a05d0d83489293cf4cfd3e0e5b723
叶宏宇 <1119819596@qq.com>
2020-03-15 10:12:40 +0800

    修改

--- 5_累加2.js
+++ 5_累加2.js
@@ -3,7 +3,7 @@ for(var i=1;i<=9;i++){
     // 暂时想不到别的办法，只能用字符串叠加通过number函数转成数字
     var k='';
     for (var j=1;j<=i;j++){
-        k+='2'
+        k+='2';
     }
     sum+=Number(k);
 }
--- 6_莱布尼茨级数.js
+++ 6_莱布尼茨级数.js
@@ -1,26 +1,25 @@
-var readline = require('readline');
-	
+var readline = require("readline");
+
 //创建readline接口实例
-var  rl = readline.createInterface({
-    input:process.stdin,
-    output:process.stdout
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
 });
 
 // question方法
-rl.question("请输入参数n:",function(n){
-    var sum = 1;
+rl.question("请输入参数n:", function(n) {
+  var sum = 1;
 
-for (var i = 1; i <= n; i++) {
+  for (var i = 1; i <= n; i++) {
     // 设k为1来重置
-    var k=1;
+    var k = 1;
     // 通过for循环算出每个式子值
-  for (var j = 1; j <= i; j++) {
-    k *= j/ (2 * j + 1);
-  }
-//   sum求和，我的sum是指作业等式右面的部分
+    for (var j = 1; j <= i; j++) {
+      k *= j / (2 * j + 1);
+    }
+    //   sum求和，我的sum是指作业等式右面的部分
     sum += k;
-}
-// 顾再此输出π值时需×2
-console.log('π值为：'+2*sum);
-}
-)
+  }
+  // 顾再此输出π值时需×2
+  console.log("π值为：" + 2 * sum);
+});

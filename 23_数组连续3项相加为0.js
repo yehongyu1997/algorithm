c3d75cfc00664696594860992e0547676662057e
叶宏宇 <1119819596@qq.com>
2020-03-16 22:48:39 +0800

    连续3项相加为0

---
+++ 23_数组连续3项相加为0.js
@@ -0,0 +1,9 @@
+var arr = [1,2,4,-3,2,3,1,-4,-2,2,4,-5];
+
+for (var i=0;i<arr.length;i++){
+   var sum =0;
+   sum=arr[i]+arr[i+1]+arr[i+2];
+   if(sum==0){
+       console.log(arr[i],arr[i+1],arr[i+2]);
+   }
+}

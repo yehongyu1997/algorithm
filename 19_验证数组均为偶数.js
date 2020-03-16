c7ef60b949870bd91acbcadba4c035c2bdf874d0
叶宏宇 <1119819596@qq.com>
2020-03-16 20:29:18 +0800

    验证数组每项是否为偶数

---
+++ 19_验证数组均为偶数.js
@@ -0,0 +1,8 @@
+var arr = [6, 10, 12, 14, 88, 81, 100, 56];
+for (var i = 0; i <arr.length; i++) {
+  if (arr[i] % 2 == 0) {
+    console.log(arr[i]+"是偶数");
+  } else {
+    console.log(arr[i]+"不是偶数");
+  }
+}

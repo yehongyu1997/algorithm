4b45e0fe8b108eb17ff4e89bf54998518dfa517e
叶宏宇 <1119819596@qq.com>
2020-03-19 09:52:09 +0800

    修改之后，利用flag

--- 19_验证数组均为偶数.js
+++ 19_验证数组均为偶数.js
@@ -1,8 +1,13 @@
 var arr = [6, 10, 12, 14, 88, 81, 100, 56];
+var flag = true;
 for (var i = 0; i <arr.length; i++) {
   if (arr[i] % 2 == 0) {
-    console.log(arr[i]+"是偶数");
-  } else {
-    console.log(arr[i]+"不是偶数");
-  }
+     flag = false;
+     break;
+  } 
+}
+if(true){
+  console.log('该数组均为偶数');
+}else{
+  console.log('该数组不均为偶数');
 }
